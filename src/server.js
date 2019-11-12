6bfa83831556bca64692e5763a6f3ac863f688f2
zarathos-sky <zarathos.sky@gmail.com>
2019-11-11 23:13:37 -0300

    first version backend

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-illat.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// req. query = acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+    app.use(express.json());
+    app.use(routes);    
+
+app.listen(3333);
